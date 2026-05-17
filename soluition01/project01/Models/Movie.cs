10c0c91e62811ec41428d8e2cc9c3ca8c7df86a6
Malakaka <malakalsenine@gmail.com>
2026-05-17 16:06:11 +0400

    Add Movie.cs in Models folder

--- soluition01/project01/Models/Movie.cs
+++ soluition01/project01/Models/Movie.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace project01.Models
 {
-    internal class Movie
+    public class Movie
     {
+      
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public int ReleaseYear { get; set; }
+
+        // Foreign Key
+      
+        public int CategoryId { get; set; }
+
+        // Navigation Property
+        // كل فيلم ينتمي لتصنيف واحد
+        public Category Category { get; set; }
+
+        // الفيلم الواحد يقدر يحتوي Reviews كثيرة
+        public List<Review> Reviews { get; set; } = new List<Review>();
+
+        // الفيلم الواحد يقدر يكون في Watchlists كثيرة
+        public List<Watchlist> Watchlists { get; set; } = new List<Watchlist>();
     }
 }
