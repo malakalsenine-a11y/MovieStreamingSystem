d5b5ce8ff6fa840c9d75e3042898df3b81e8ee8f
Malakaka <malakalsenine@gmail.com>
2026-05-17 16:08:23 +0400

    Add Watchlist.cs in Models folder

--- soluition01/project01/Models/Watchlist.cs
+++ soluition01/project01/Models/Watchlist.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace project01.Models
 {
-    internal class Watchlist
+    public class Watchlist
     {
+        // Foreign Key للمستخدم
+        public int UserId { get; set; }
+
+        // Navigation Property
+        public User User { get; set; }
+
+        // Foreign Key للفيلم
+        public int MovieId { get; set; }
+
+        // Navigation Property
+        public Movie Movie { get; set; }
+
+        // تاريخ إضافة الفيلم
+        public DateTime AddedDate { get; set; } = DateTime.Now;
     }
 }
