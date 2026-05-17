e59d60d5f7aba7dcfb0c2955f990073a96589381
Malakaka <malakalsenine@gmail.com>
2026-05-17 16:07:29 +0400

    Add Review.cs in Models folder

--- soluition01/project01/Models/Review.cs
+++ soluition01/project01/Models/Review.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace project01.Models
 {
-    internal class Review
+    public class Review
     {
+     
+        public int Id { get; set; }
+
+        public string Comment { get; set; }
+
+        // التقييم من 1 إلى 5
+        public int Rating { get; set; }
+
+        // Foreign Key للمستخدم
+        public int UserId { get; set; }
+
+        // Navigation Property
+        public User User { get; set; }
+
+        // Foreign Key للفيلم
+        public int MovieId { get; set; }
+
+        // Navigation Property
+        public Movie Movie { get; set; }
     }
 }
