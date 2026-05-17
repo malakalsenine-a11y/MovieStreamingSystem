98078b931ae91f2d4b8ab829f4a702b49f6ebaf7
Malakaka <malakalsenine@gmail.com>
2026-05-17 15:35:12 +0400

    Add Models layer with entity classes (User, Movie, Category, Review, Watchlist) to structure database schema

--- soluition01/project01/obj/Debug/net8.0/project01.AssemblyInfo.cs
+++ soluition01/project01/obj/Debug/net8.0/project01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee2ca2bce1651cf1d3886179c22cef1a1d5d70c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("project01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
